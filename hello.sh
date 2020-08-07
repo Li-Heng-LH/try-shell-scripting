2e4c75f21501744ffe6641149b4b0e7c5f512a93
Li Heng <liheng6567@gmail.com>
2020-08-07 17:44:30 +0800

    command sub, arithmetic ops, string concat

--- hello.sh
+++ hello.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
-# This is a comment!
+echo
+
+# echo and variales
 greeting="hello" #Cannot have spaces around =
-
 echo '$greeting, world (planet)!'
-echo
-echo
 echo "$greeting, world (planet)!"
+echo
 
+# command substitution
+pwd_result=$(pwd)
+echo $pwd_result
+echo
+
+# arithmetic operations
+a=2
+b=3
+echo $((a+b))
+echo $(($a+$b))
+c=$((a*b))
+echo $c
+echo
+
+# String manipulation
+a="con"
+b="cated"
+c=$a$b
+echo $a$b
+echo $c
+echo ${#c} # length of string
+
+echo
