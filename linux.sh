4b6dfb3c740235207a96e411b817bfe5bee66824
Li Heng <liheng6567@gmail.com>
2020-08-28 00:43:06 +0800

    grep with highlight

--- linux.sh
+++ linux.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "hello" | wc
-
+echo
 cat -n poems.txt | tail -n5 # -n Number the output lines, starting at 1.
-
+echo
+grep "the" -n poems.txt --colour # grep with color, with line number stated
+echo
