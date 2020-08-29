cbe87ae59e6e17dcecd9428c64996fd59176fda4
Li Heng <liheng6567@gmail.com>
2020-08-29 20:27:51 +0800

    preserve highlighting after piping into head

--- linux.sh
+++ linux.sh
@@ -6,3 +6,6 @@ cat -n poems.txt | tail -n5 # -n Number the output lines, starting at 1.
 echo
 grep "the" -n poems.txt --colour # grep with color, with line number stated
 echo
+grep "the" -n poems.txt --color=always | head -n1 # to preserve highlighting after piping into head
+echo
+
