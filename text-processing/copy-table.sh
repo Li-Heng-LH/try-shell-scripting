db4da4a9eee17a62fea3dca4fc47cc668ef7ee53
Li Heng <liheng6567@gmail.com>
2021-07-04 14:54:56 +0800

    Yes! Copied tables

--- text-processing/copy-table.sh
+++ text-processing/copy-table.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-awk '/##Table 2/ {exit} /##Table 1/ {f=1; next} f' old.txt > result.txt
+#help: https://unix.stackexchange.com/questions/180663/how-to-select-first-occurrence-between-two-patterns-including-them
+
+# Tale 1st table
+awk '/##Table 2/ {exit} /##Table 1/ {f=1; next} f' old.txt > tmp.txt
+# Tale last table
+sed '/##Total/,/ABC/!d;/ABC/q' old.txt >> tmp.txt
+
+shortDash=$(echo $(printf '%0.s-' {1..89}))
+longDash=$(echo $(printf '%0.s-' {1..129}))
+
+echo $shortDash >> tmp.txt
+
+sed "s/$longDash/$shortDash/g" tmp.txt > result.txt
+rm tmp.txt
